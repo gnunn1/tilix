ca384610db209e008f9a853a3ea0d16a5c94674e
Philipp Wolfer <ph.wolfer@gmail.com>
2016-03-26 15:58:07 +0100

    i18n: Localize the about dialog.

--- source/gx/terminix/constants.d
+++ source/gx/terminix/constants.d
@@ -4,6 +4,7 @@
  */
 module gx.terminix.constants;
 
+import std.format;
 import std.path;
 
 import gx.i18n.l10n;
@@ -34,13 +35,15 @@ enum APPLICATION_NAME = "Terminix";
 enum APPLICATION_VERSION = "0.55.0";
 enum APPLICATION_AUTHOR = "Gerald Nunn";
 enum APPLICATION_COPYRIGHT = "Copyright \xc2\xa9 2016 " ~ APPLICATION_AUTHOR;
-enum APPLICATION_COMMENTS = "A VTE based terminal emulator for Linux";
-enum APPLICATION_LICENSE = "This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.";
+enum APPLICATION_COMMENTS = N_("A VTE based terminal emulator for Linux");
+enum APPLICATION_LICENSE = N_("This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.");
 enum APPLICATION_ICON_NAME = "utilities-terminal";
 
-immutable string[] APPLICATION_AUTHORS = ["Gerald Nunn"];
+immutable string[] APPLICATION_AUTHORS = [APPLICATION_AUTHOR];
 string[] APPLICATION_CREDITS = [
-    "GTK VTE widget team, Terminix would not be possible without their work", "GtkD for providing such an excellent GTK wrapper", "Dlang.org for such an excellent language, D"
+    N_("GTK VTE widget team, Terminix would not be possible without their work"),
+    N_("GtkD for providing such an excellent GTK wrapper"),
+    N_("Dlang.org for such an excellent language, D")
 ];
 immutable string[] APPLICATION_ARTISTS = [];
 immutable string[] APPLICATION_DOCUMENTERS = [""];
