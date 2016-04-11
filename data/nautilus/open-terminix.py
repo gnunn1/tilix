a61cedb8809c1a2102e3c80c82829f9fc9cb01d2
Philipp Wolfer <ph.wolfer@gmail.com>
2016-04-11 23:34:49 +0200

    i18n: Fixed localization of Nautilus extension
    The strings where not displayed in the current locale. The extension is running in Python 2, thus strings need to be marked as UTF-8 strings.

--- data/nautilus/open-terminix.py
+++ data/nautilus/open-terminix.py
@@ -16,38 +16,38 @@ gi.require_version('Nautilus', '3.0')
 from gi.repository import Nautilus, GObject, Gio
 
 class OpenTerminixExtension(GObject.GObject, Nautilus.MenuProvider):
-        
+
     def _open_terminal(self, file):
         gfile = Gio.File.new_for_uri(file.get_uri())
         filename = gfile.get_path();
         terminal = "terminix"
 
-        #print "Opening file:", filename 
+        #print "Opening file:", filename
         os.system('%s -w "%s" &' % (terminal, filename))
-        
+
     def menu_activate_cb(self, menu, file):
         self._open_terminal(file)
-        
-    def menu_background_activate_cb(self, menu, file): 
+
+    def menu_background_activate_cb(self, menu, file):
         self._open_terminal(file)
-       
+
     def get_file_items(self, window, files):
         if len(files) != 1:
             return
-        
+
         file = files[0]
         if not file.is_directory() or file.get_uri_scheme() != 'file':
             return
-        
+
         item = Nautilus.MenuItem(name='NautilusPython::openterminal_file_item',
-                                 label=_('Open in Terminix…'),
-                                 tip=_('Open Terminix In %s') % file.get_name())
+                                 label=_(u'Open in Terminix…'),
+                                 tip=_(u'Open Terminix In %s') % file.get_name())
         item.connect('activate', self.menu_activate_cb, file)
         return item,
 
     def get_background_items(self, window, file):
         item = Nautilus.MenuItem(name='NautilusPython::openterminal_item',
-                                 label=_('Open Terminix Here…'),
-                                 tip=_('Open Terminix In This Directory'))
+                                 label=_(u'Open Terminix Here…'),
+                                 tip=_(u'Open Terminix In This Directory'))
         item.connect('activate', self.menu_background_activate_cb, file)
         return item,
