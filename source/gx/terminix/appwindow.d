e99c6217e29ce95a8148628e9563d8ff1d53eb52
gnunn1 <gerald.b.nunn@gmail.com>
2016-05-06 19:52:47 -0400

    Fix #303 plus improve transparent scrollbar handling

--- source/gx/terminix/appwindow.d
+++ source/gx/terminix/appwindow.d
@@ -677,6 +677,21 @@ private:
             fullscreen();
         }
     }
+    
+    static if (MANUAL_BACKGROUND_DRAW) {
+        bool onWindowDraw(Scoped!Context cr, Widget w) {
+            // Only works for GTK 3.20, default is to use CSS classes to give widgets
+            // backgrounds, see issue #303
+            if (getAppPaintable) {
+                if (cast(Popover)getFocus() !is null) return false;
+                Widget child = getChild();
+                GdkRectangle rect;
+                child.getAllocation(rect);
+                StyleContext.renderBackground(getStyleContext(), cr, rect.x, rect.y, rect.width, rect.height);
+            } 
+            return false;
+        }
+    }
 
     void onCompositedChanged(Widget) {
         trace("Composite changed");
@@ -816,6 +831,9 @@ public:
 
         addOnDelete(&onWindowClosed);
         addOnDestroy(&onWindowDestroyed);
+        static if (MANUAL_BACKGROUND_DRAW) {
+            addOnDraw(&onWindowDraw);
+        }
         addOnRealize(delegate (Widget) {
             if (terminix.getGlobalOverrides().x > 0) {
                 move(terminix.getGlobalOverrides().x, terminix.getGlobalOverrides().y);
