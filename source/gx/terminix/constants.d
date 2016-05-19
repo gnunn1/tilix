1264eb33e78405db99b6288a3e8e35dc0b889494
gnunn1 <gerald.b.nunn@gmail.com>
2016-05-18 19:09:33 -0600

    Bump version number for minor release

--- source/gx/terminix/constants.d
+++ source/gx/terminix/constants.d
@@ -47,7 +47,7 @@ enum APPLICATION_ID = "com.gexperts.Terminix";
 
 // Application values used in About Dialog
 enum APPLICATION_NAME = "Terminix";
-enum APPLICATION_VERSION = "0.60.1";
+enum APPLICATION_VERSION = "0.60.2";
 enum APPLICATION_AUTHOR = "Gerald Nunn";
 enum APPLICATION_COPYRIGHT = "Copyright \xc2\xa9 2016 " ~ APPLICATION_AUTHOR;
 enum APPLICATION_COMMENTS = N_("A VTE based terminal emulator for Linux");
