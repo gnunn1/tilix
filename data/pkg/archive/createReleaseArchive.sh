b5729ed6116df1d76177dbfad83c9c04c0e8e23f
gnunn1 <gerald.b.nunn@gmail.com>
2016-12-12 18:44:28 -0500

    Script for creating release archive

---
+++ data/pkg/archive/createReleaseArchive.sh
@@ -0,0 +1,21 @@
+export TERMINIX_ARCHIVE_PATH="/tmp/terminix/archive";
+
+rm -rf ${TERMINIX_ARCHIVE_PATH}
+
+CURRENT_DIR=$(pwd)
+
+echo "Building application..."
+cd ../../..
+dub build --build=release
+
+./install.sh ${TERMINIX_ARCHIVE_PATH}/usr
+
+# Remove compiled schema
+rm ${TERMINIX_ARCHIVE_PATH}/usr/share/glib-2.0/schemas/gschemas.compiled
+
+echo "Creating archive"
+cd ${TERMINIX_ARCHIVE_PATH}
+zip -r terminix.zip *
+
+cp terminix.zip ${CURRENT_DIR}/terminix.zip
+cd ${CURRENT_DIR}
