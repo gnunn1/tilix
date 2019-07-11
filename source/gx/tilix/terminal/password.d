9d0c052758faedd3f3acaf350777cb4fa35bd44b
myfreeweb <greg@unrelenting.technology>
2019-07-11 13:04:58 +0300

    Use & when passing function references to HashTable constructor
    ldc 1.16.0 complains about trying to call `g_str_hash` / `g_str_equal` with no arguments

--- source/gx/tilix/terminal/password.d
+++ source/gx/tilix/terminal/password.d
@@ -172,7 +172,7 @@ private:
                 string uuid = randomUUID().toString();
                 pending[uuid] = c;
                 import gtkc.glib;
-                HashTable attributes = new HashTable(g_str_hash, g_str_equal);
+                HashTable attributes = new HashTable(&g_str_hash, &g_str_equal);
                 immutable(char*) uuidz = toStringz(uuid);
                 attributes.insert(cast(void*)attrID, cast(void*)uuidz);
                 attributes.insert(cast(void*)attrDescription, cast(void*)descriptionValue);
@@ -285,7 +285,7 @@ private:
 
     HashTable createHashTable() {
         import gtkc.glib;
-        return new HashTable(g_str_hash, g_str_equal);
+        return new HashTable(&g_str_hash, &g_str_equal);
     }
 
     void createSchema() {
